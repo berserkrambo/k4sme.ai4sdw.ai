669128714e40bef2baf107d6fc0de6973695b38d
berserkrambo <berserkrambo@gmail.com>
2022-11-16 15:19:12 +0100

    init

--- tests/sim/runner.py
+++ tests/sim/runner.py
@@ -19,25 +19,25 @@ def bootstrap():
 
 
 def send_machine_entities():
-    sampler = MachineSampler(pool_size=1)
-    sampler.sample(samples_n=1, sampling_rate=5)
-    print(sampler.make_device_entity(1))
+    # sampler = MachineSampler(pool_size=1)
+    # sampler.sample(samples_n=1, sampling_rate=5)
+    # print(sampler.make_device_entity(1))
 
 
-    # n = random.randint(0, 10)
-    # boxes = ""
-    # for i in range(n):
-    #     bbox = f"{random.randint(0, 100)},{random.randint(0, 100)},{random.randint(0, 100)},{random.randint(0, 100)}"
-    #     boxes = boxes + bbox
-    #     if i < n - 1:
-    #         boxes = boxes + "-"
-    #
-    # myobj = {'entities': [{'id': 'urn:ngsi-ld:Machine:1', 'type': 'Machine', 'bbox': {'type': 'Text', 'value': boxes}}], 'actionType': 'append'}
-    # url = 'http://localhost:1026/v2/op/update'
-    # headers = {"Content-Type": "application/json", "fiware-service": "csic"}
-    # x = post(url, json=myobj, headers=headers)
-    # print(x.text)
-    # time.sleep(5)
+    n = random.randint(0, 10)
+    boxes = ""
+    for i in range(n):
+        bbox = f"{random.randint(0, 100)},{random.randint(0, 100)},{random.randint(0, 100)},{random.randint(0, 100)}"
+        boxes = boxes + bbox
+        if i < n - 1:
+            boxes = boxes + "-"
+
+    myobj = {'entities': [{'id': 'urn:ngsi-ld:Machine:1', 'type': 'Machine', 'bbox': {'type': 'Text', 'value': boxes}}], 'actionType': 'append'}
+    url = 'http://localhost:1026/v2/op/update'
+    headers = {"Content-Type": "application/json", "fiware-service": "csic"}
+    x = post(url, json=myobj, headers=headers)
+    print(x.text)
+    time.sleep(5)
 
 def run():
     services_running = False
